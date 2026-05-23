7fdbaf59d271a0f50280723c9acbb7b3e1062042
GJ Enosh <enoshgollapalli@gmail.com>
2026-05-23 21:54:27 +0530

    chore: trigger CI re-run

--- tests/test_cleaning.py
+++ tests/test_cleaning.py
@@ -3224,4 +3224,6 @@ class TestSelectColumns:
 
         df = pd.DataFrame({"first": [None], "last": [None]})
         result = ar.combine_columns(df, subset=["first", "last"], output_column="full")
-        assert pd.isna(result["full"][0]) 
+        assert pd.isna(result["full"][0])
+        
+    # trigger ci re-run    
